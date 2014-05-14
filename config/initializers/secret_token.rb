c645c685640c64b2733c179c99355954bb953bdf
panuinth <panu@panu.in.th>
2014-05-14 00:23:15 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Peerakit::Application.config.secret_key_base = 'fda7ffda917bb621f287bbde4484a791ac249c18c9d7ef48c395f1204076b2019e02ef18e228272dd73f60c8ec3bb2aee09865ec794c65fb969dbc5b371b06f8'
